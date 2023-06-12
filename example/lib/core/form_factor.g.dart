ceec81fe7152d479134161824b52462714accb22
Laco <laurent.couturier@edenred.com>
2023-06-12 17:56:17 +0200

    update package

--- example/lib/core/form_factor.g.dart
+++ example/lib/core/form_factor.g.dart
@@ -6,108 +6,20 @@ part of 'form_factor.dart';
 // EnumPatternMatchingGenerator
 // **************************************************************************
 
-extension ExtensionFormFactor on FormFactor {
-  /// Whether this is equal to  FormFactor.plastic.
-  bool get isPlastic => this == FormFactor.plastic;
+/*Could not format because the source could not be parsed:
 
-  /// Whether this is equal to  FormFactor.virtual.
-  bool get isVirtual => this == FormFactor.virtual;
-
-  /// Use when method when you want to perform some action based on the enum
-  ///
-  /// ```dart
-  /// FormFactor value = FormFactor.plastic;
-  /// final result = value.when(
-  ///   plastic: () => 'plastic',
-  ///   virtual: () => 'virtual',
-  /// );
-  /// ```
-  T when<T>({
-    required T Function() plastic,
-    required T Function() virtual,
-  }) {
-    switch (this) {
-      case FormFactor.plastic:
-        return plastic();
-      case FormFactor.virtual:
-        return virtual();
-    }
-  }
-
-  /// Use map method when you want to perform some action based on the enum
-  ///
-  /// ```dart
-  /// FormFactor value = FormFactor.plastic;
-  /// final result = value.map(
-  ///   plastic: (e) => e.toString(),
-  ///   virtual: (e) => e.toString(),
-  /// );
-  /// ```
-  T map<T>({
-    required T Function(FormFactor) plastic,
-    required T Function(FormFactor) virtual,
-  }) {
-    switch (this) {
-      case FormFactor.plastic:
-        return plastic(this);
-      case FormFactor.virtual:
-        return virtual(this);
-    }
-  }
-
-  /// Use mayBeWhen method when you want to perform some action based on the enum
-  ///
-  /// Throws an [ArgumentError] if all parameters are null
-  /// ```dart
-  /// FormFactor value = FormFactor.plastic;
-  /// final result = value.mayBeWhen(
-  ///   plastic: () => 'plastic',
-  ///   orElse: () => 'default'
-  /// );
-  /// ```
-  T mayBeWhen<T>({
-    T Function()? plastic,
-    T Function()? virtual,
-    required T Function() orElse,
-  }) {
-    assert(() {
-      if (plastic == null && virtual == null) {
-        throw ArgumentError('check for at least one case');
-      }
-      return true;
-    }());
-    final items = {
-      FormFactor.plastic: plastic,
-      FormFactor.virtual: virtual,
-    };
-    return items[this]?.call() ?? orElse();
-  }
-
-  /// Use mayBeMap method when you want to perform some action based on the enum
-  ///
-  /// Throws an [ArgumentError] if all parameters are null
-  /// ```dart
-  /// FormFactor value = FormFactor.plastic;
-  /// final result = value.mayBeMap(
-  ///   plastic: (e) => e.toString(),
-  ///   orElse: () => 'default'
-  /// );
-  /// ```
-  T mayBeMap<T>({
-    T Function(FormFactor)? plastic,
-    T Function(FormFactor)? virtual,
-    required T Function() orElse,
-  }) {
-    assert(() {
-      if (plastic == null && virtual == null) {
-        throw ArgumentError('check for at least one case');
-      }
-      return true;
-    }());
-    final items = {
-      FormFactor.plastic: plastic,
-      FormFactor.virtual: virtual,
-    };
-    return items[this]?.call(this) ?? orElse();
-  }
-}
+line 15, column 8 of .: Expected an identifier.
+   ╷
+15 │ return switch (this) {
+   │        ^^^^^^
+   ╵
+line 15, column 1 of .: Expected to find ';'.
+   ╷
+15 │ return switch (this) {
+   │ ^^^^^^
+   ╵
+line 16, column 1 of .: Expected to find 'case'.
+   ╷
+16 │ plastic => plastic(),
+   │ ^^^^^^^
+   ╵*/
